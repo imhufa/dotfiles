d87de045a388dc888166ecfabc4ebd77efb3611c
imhufa <hufalab@gmail.com>
2024-08-27 04:57:35 -0700

    .

--- scripts/rmfirstLine.sh
+++ scripts/rmfirstLine.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/zsh
 
-tree -a --noreport -L 1 $HOME/dotfiles > ./tree.md; wait
-tail -n +2 "tree.md" > "tree.tmp"; mv "tree.tmp" "tree.md"; wait
-cat ./header.md > ./readme.md; wait
-echo -e '\n<b>dotfiles</b>' >> ./readme.md; wait
-cat ./tree.md >> ./readme.md; wait
-echo -e '<br>\n<br>\n<br>\n<br>' >> ./readme.md
-echo '<i><b>[updated]: </b>'$(date +%D)'</i>' >> ./readme.md
+echo -n 'lineFile: '
+read lineFile
+tmpFile=$lineFile'.tmp'
+
+tail -n +2 "$lineFile" > "$tmpFile"; mv $(echo $tmpFile) $(echo $lineFile)
 
